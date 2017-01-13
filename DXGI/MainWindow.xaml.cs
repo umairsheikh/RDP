4e91a39749eff29a36903a64a3e99aaf4d2f696e
Umair Sheikh <umairsly@gmail.com>
2017-01-13 09:39:26 +0000

    Added Client connection

--- DXGI/MainWindow.xaml.cs
+++ DXGI/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Threading.Tasks;
 using Managers.LiveControl.Client;
 
 using Providers.LiveControl.Client;
+using Network;
+using Network.Messages.Nova;
 
 namespace DXGI_DesktopDuplication
 {
@@ -24,7 +26,9 @@ namespace DXGI_DesktopDuplication
         public static UpdateUI RefreshUI;
         private Thread duplicateThread = null;
 
-        public NovaManager NovaManager { get { return Managers.NovaServer.Instance.NovaManager; } }
+
+        public Managers.Nova.Client.NovaManager NovaManagerClient { get { return Managers.NovaClient.Instance.NovaManager; } }
+        public NovaManager NovaManagerServer { get { return Managers.NovaServer.Instance.NovaManager; } }
         public Managers.LiveControl.Server.LiveControlManager LiveControlManagerServer { get { return Managers.NovaServer.Instance.LiveControlManager; } }
         public LiveControlManager LiveControlManagerClient { get { return Managers.NovaClient.Instance.LiveControlManager; } }
 
@@ -33,20 +37,14 @@ namespace DXGI_DesktopDuplication
         {
             InitializeComponent();
 
-         
-
-            //LiveControlManagerServer
 
             RefreshUI = UpdateImage;
-            //duplicateThread = new Thread(Demo);
 
             //test code here
             Console.WriteLine("{0}, {1}", SystemParameters.WorkArea.Width, SystemParameters.WorkArea.Height);
             Console.WriteLine("{0}, {1}", SystemParameters.PrimaryScreenWidth, SystemParameters.PrimaryScreenHeight);
             Console.WriteLine(Marshal.SizeOf(typeof(Vertex)));
 
-            // duplicationManager = DuplicationManager.GetInstance(Dispatcher);
-            //  LiveControlManagerServer.
 
         }
 
@@ -74,18 +72,61 @@ namespace DXGI_DesktopDuplication
         public async Task InitNetworkManager()
         {
 
-            NovaManager.OnIntroducerRegistrationResponded += NovaManager_OnIntroducerRegistrationResponded;
-            NovaManager.OnNewPasswordGenerated += new EventHandler<PasswordGeneratedEventArgs>(ServerManager_OnNewPasswordGenerated);
-            NovaManager.Network.OnConnected += new EventHandler<Network.ConnectedEventArgs>(Network_OnConnected);
+            NovaManagerClient.OnIntroductionCompleted += new EventHandler<IntroducerIntroductionCompletedEventArgs>(ClientManager_OnIntroductionCompleted);
+            NovaManagerClient.OnNatTraversalSucceeded += new EventHandler<Network.NatTraversedEventArgs>(ClientManager_OnNatTraversalSucceeded);
+            NovaManagerClient.OnConnected += new EventHandler<ConnectedEventArgs>(ClientManager_OnConnected);
 
-            PasswordGeneratedEventArgs passArgs = await NovaManager.GeneratePassword();
+
+
+            NovaManagerServer.OnIntroducerRegistrationResponded += NovaManager_OnIntroducerRegistrationResponded;
+            NovaManagerServer.OnNewPasswordGenerated += new EventHandler<PasswordGeneratedEventArgs>(ServerManager_OnNewPasswordGenerated);
+            NovaManagerServer.Network.OnConnected += new EventHandler<Network.ConnectedEventArgs>(Network_OnConnected);
+
+            PasswordGeneratedEventArgs passArgs = await NovaManagerServer.GeneratePassword();
             LabelPassword.Content = passArgs.NewPassword;
-            IntroducerRegistrationResponsedEventArgs regArgs = await NovaManager.RegisterWithIntroducerAsTask();
+            IntroducerRegistrationResponsedEventArgs regArgs = await NovaManagerServer.RegisterWithIntroducerAsTask();
             LabelNovaId.Content = regArgs.NovaId;
             Status.Content = "Host is live";
 
-           // LiveControlManagerServer.RunThreadToSendFrames(this.Dispatcher);
+            // LiveControlManagerServer.RunThreadToSendFrames(this.Dispatcher);
 
+        }
+
+        void ClientManager_OnConnected(object sender, ConnectedEventArgs e)
+        {
+            //  ButtonConnect.Set(() => ButtonConnect.Text, "Connected.");
+            remoteConnection.Content = "Connected to remote";
+            // this.Dispose();
+        }
+
+        private void ClientManager_OnIntroductionCompleted(object sender, IntroducerIntroductionCompletedEventArgs e)
+        {
+            switch (e.Result)
+            {
+                case Network.Messages.Nova.ResponseIntroducerIntroductionCompletedMessage.Result.Allowed:
+                    // Do nothing, expect OnNatTraversalSucceeded() to be raised shortly
+                    break;
+
+                case Network.Messages.Nova.ResponseIntroducerIntroductionCompletedMessage.Result.Denied:
+                    switch (e.DenyReason)
+                    {
+                        case ResponseIntroducerIntroductionCompletedMessage.Reason.WrongPassword:
+                            //TextBox_Password.Set(() => TextBox_Password.Text, String.Empty); // clear the password box for re-entry
+                            remoteConnection.Content = "Enter correct password/login";
+                            MessageBox.Show("Please enter the correct password.");
+                            break;
+                        case ResponseIntroducerIntroductionCompletedMessage.Reason.Banned:
+                            MessageBox.Show("You have been banned for trying to connect too many times.");
+                            break;
+                    }
+                    break;
+            }
+        }
+
+        private void ClientManager_OnNatTraversalSucceeded(object sender, NatTraversedEventArgs e)
+        {
+            // ButtonConnect.Set(() => ButtonConnect.Text, "Connecting to " + TextBox_Id.Text + " ...");
+            remoteConnection.Content = "Connecting to machine..";
         }
 
 
@@ -177,10 +218,10 @@ namespace DXGI_DesktopDuplication
 
         }
 
-        private void ConnectRemote_Click(object sender, RoutedEventArgs e)
+        private async void ConnectRemote_Click(object sender, RoutedEventArgs e)
         {
             LiveControlManagerClient.OnScreenshotReceived += new EventHandler<ScreenshotMessageEventArgs>(LiveControlManager_OnScreenshotReceived);
-
+            await NovaManagerClient.RequestIntroductionAsTask(RID.Text, PWD.Text);
 
             //if (duplicateThread.ThreadState == System.Threading.ThreadState.Unstarted)
             //{
@@ -195,20 +236,15 @@ namespace DXGI_DesktopDuplication
 
         private void LiveControlManager_OnScreenshotReceived(object sender, ScreenshotMessageEventArgs e)
         {
-            // var screenshot = e.Screenshot;
-
             var screenshot = e.Screenshot;
+
             using (var stream = new System.IO.MemoryStream(screenshot.Image))
             {
 
-                //this.Dispatcher.BeginInvoke(MainWindow.RefreshUI,LiveControlManagerServer.);
-
-                //Show image with current render code
-
-                // Image image = Image.FromStream(stream);
-                //Application.DoEvents();
-                //this.BackgroundImage = image;
-                /*   if (ShowRegionOutlines)
+                /*   Image image = Image.FromStream(stream);
+                   //Application.DoEvents();
+                   //this.BackgroundImage = image;
+                   if (ShowRegionOutlines)
                    {
                        var gfx = gdiScreen1.CreateGraphics();
                        gfx.DrawLine(pen, new Point(e.Screenshot.Region.X, e.Screenshot.Region.Y), new Point(e.Screenshot.Region.X + e.Screenshot.Region.Width, e.Screenshot.Region.Y));
@@ -220,9 +256,7 @@ namespace DXGI_DesktopDuplication
                    gdiScreen1.Draw(image, screenshot.Region);
                }
 
-               LiveControlManager.RequestScreenshot();
-               */
-                //Trace.WriteLine(String.Format("Processed Image #{0}, Size: {1} KB", e.Screenshot.Number, GetKBFromBytes(e.Screenshot.Image.Length)));
+               LiveControlManager.RequestScreenshot();*/
             }
         }
     }
